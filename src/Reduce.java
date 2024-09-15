024ece9ff823931556bbde86c7b198bb46a27e67
Chihana Kashiwabara <c.kashiwabara@mail.utoronto.ca>
2024-09-14 21:23:44 -0400

    Add Reduce

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,23 @@
 public class Reduce {
+    public static void main(String[] args) {
+        int start = 100;
+        int steps = 0;
+
+        while (start > 0){
+            boolean even = start % 2 == 0;
+            boolean odd = start % 2 != 0;
+
+            if (even){
+                start = start / 2;
+                steps++;
+            }
+
+            if (odd) {
+                start--;
+                steps++;
+            }
+        }
+
+        System.out.println(steps);
+    }
 }
